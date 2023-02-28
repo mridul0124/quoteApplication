01ea18150cf9a70a81829e7be3b07cbd1bf82206
mridul764 <54048418+mridul764@users.noreply.github.com>
2023-02-28 23:35:02 +0530

    my commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useEffect,useState} from 'react';
+import axios from "axios";
 import './App.css';
 
+const url = "https://api.adviceslip.com/advice";
 function App() {
+ const[quote,setQuote] = useState([]);
+
+  
+  const getQuote = async () =>{
+    const res = await axios.get(url);
+    const quoteData = res.data.slip;
+    //console.log(quoteData);
+    setQuote(quoteData);
+  } ;
+  
+  useEffect(()=>{
+    getQuote();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+      <div className='body'>
+      <h1 className='heading'>Quote</h1>
+      {/* <div className='image-bg' style={{
+        backgroundImage:`url(${Image})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        width: '100vw',
+        height: '100vh'
+        // backgroundSize:'contain',
+        // backgroundRepeat:'no-repeat',
+        // width:'120%',
+        // height:'100vh',
+        }}></div> */}
+      
+        <div className='quote-container'>
+          <h4 className='quote-text'>{quote.advice}</h4>
+          <button className='click-btn' onClick={getQuote}>Next Quote</button>
+        </div>
+      </div>
     </div>
   );
 }
